bc9b1625f31fc57cc9138ffeacf765e17cc0f48c
Chantal2015 <chantalsaadhajjar@gmail.com>
2024-10-18 15:26:56 +0300

    Exerice 1 - Partie 2

--- Ex1/ex1.cpp
+++ Ex1/ex1.cpp
@@ -40,7 +40,7 @@ double trouverNoteMaximale(const vector<double>& v) {
 void ajouterNotes(vector<double>& v, int n) {
 	saisirNotes(v, n);
 }
-int main() {
+int main_p1() {
 	
 	int N1, N2;
 	vector<double> notes;
@@ -68,6 +68,94 @@ int main() {
 	cout << "Nouvelle moyenne des notes : " << moyenne << endl;
 	// Recherche et affichage de la nouvelle note maximale
 	noteMax = trouverNoteMaximale(notes);
+	cout << "Nouvelle note maximale : " << noteMax << endl;
+	return 0;
+}
+// partie 2
+
+
+
+
+const int N1 = 5;
+const int N2 = 3;
+
+void saisirNotes(array<double,N1>& arr, int& count ) {
+	
+	for (int i = 0; i < N1; i++) {
+		cout << "Entrer la note de l'etudiant " << i + 1 << " :";
+		cin >> arr[i];
+		count++;
+	}
+	
+
+}
+
+void etendreTableau(const array<double, N1>& arr1, array<double, N1 + N2>& arr2, int& count) {
+	for (int i = 0; i < N1; ++i) {
+		arr2[i] = arr1[i];
+	
+	}
+}
+
+void afficherNotes(const array<double, N1 + N2>& arr, int count) {
+	for (int i = 0; i < count; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+void ajouterNotes(array < double, N1 + N2>& notesEtendues, int& count) {
+	for (int i = N1; i < N1+N2; i++) {
+		cout << "Entrer la note de l'etudiant " << i+1 << " :";
+		cin >> notesEtendues[i];
+		count++;
+	}
+	
+}
+double calculerMoyenne(const array<double, N1 + N2>& arr, int count) {
+	double m = 0;
+	for (size_t i = 0; i < count; i++) {
+		m += arr[i];
+	}
+	return m / count;
+}
+double trouverNoteMaximale(const array<double, N1 + N2>& arr, int count) {
+	double max = 0;
+	for (size_t i = 0; i < count; i++) {
+		if (arr[i] > max)
+			max = arr[i];
+	}
+	return max;
+}
+
+int main() {
+	array<double, N1> notesInitiales = { 0 };
+	int count = 0; // contient le nombre d’etudiants saisi
+	// Saisie des notes initiales
+	cout << "Saisie des notes des " << N1 << " etudiants de la classe initiale." << endl;
+		saisirNotes(notesInitiales, count);
+	// Extension du tableau pour inclure les nouveaux	etudiants
+		array<double, N1 + N2> notesEtendues = { 0 };
+	etendreTableau(notesInitiales, notesEtendues, count);
+	// Affichage des notes
+	afficherNotes(notesEtendues, count);
+	// Calcul et affichage de la moyenne des notes
+	double moyenne = calculerMoyenne(notesEtendues, count);
+	cout << "Moyenne des notes : " << fixed << setprecision(2)
+		<< moyenne << endl;
+	// Recherche et affichage de la note maximale
+	double noteMax = trouverNoteMaximale(notesEtendues,count);
+	cout << "Note maximale : " << noteMax << endl;
+	// Ajout des notes des nouveaux etudiants
+	cout << "Saisie des notes des " << N2 << " nouveaux etudiants." << endl;
+		ajouterNotes(notesEtendues, count);
+	 
+		// Affichage des notes apres ajout
+		afficherNotes(notesEtendues, count);
+	// Recalcul et affichage de la nouvelle moyenne des notes
+	moyenne = calculerMoyenne(notesEtendues, count);
+	cout << "Nouvelle moyenne des notes : " << moyenne << 		endl;
+	// Recherche et affichage de la nouvelle note maximale
+	noteMax = trouverNoteMaximale(notesEtendues, count);
 	cout << "Nouvelle note maximale : " << noteMax << endl;
 	return 0;
 }
